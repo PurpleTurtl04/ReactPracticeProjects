1bc9b325d371a23b54f7feb4cb195eb00edc1caf
Nick Edwards <nledwa01@gmail.com>
2025-12-12 16:41:44 -0600

    Set up projects for Next js tutorials

---
+++ NextJS-ReactFoundations/app/page.jsx
@@ -0,0 +1,18 @@
+import LikeButton from './like-button';
+
+export default function HomePage() {
+    const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+    return (
+        <div>
+            <header title='Develop. Preview. Ship.' />
+            <ul>
+                {names.map((name) => (
+                    <li key='{name}'>{name}</li>
+                ))}
+            </ul>
+
+            <LikeButton />
+        </div>
+    );
+}
